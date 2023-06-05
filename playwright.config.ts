779f90c1b62a9ae18ffe5f39be1c778c0f23aebc
Daniel Leroux <daniel.leroux@siemens.com>
2023-06-05 13:53:33 +0200

    add navigation time

--- playwright.config.ts
+++ playwright.config.ts
@@ -36,6 +36,7 @@ const config: PlaywrightTestConfig = {
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
+    navigationTimeout: 250,
     /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
     actionTimeout: 0,
     /* Base URL to use in actions like `await page.goto('/')`. */
