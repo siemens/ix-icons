2ced5d45b06b85c2d2c9756b261e6a4660815f08
Daniel Leroux <daniel.leroux@siemens.com>
2023-09-18 11:26:46 +0200

    feat: replace icon font implementation

--- gulpfile.js
+++ gulpfile.js
@@ -8,71 +8,8 @@
  */
 
 const gulp = require('gulp');
-const rename = require('gulp-rename');
-const iconfont = require('gulp-iconfont');
-const consolidate = require('gulp-consolidate');
-
-const fontName = 'ix-icons';
-const className = 'glyph';
-const template = 'ix-style';
-const templateInternal = 'ix-style-internal';
-
-const timestamp = Math.round(Date.now() / 1000);
 
 gulp.task(
   'symbols',
-  gulp.series(() =>
-    gulp
-      .src('./svg/*.svg')
-      .pipe(gulp.dest('dist-css/svg'))
-      .pipe(
-        iconfont({
-          fontName,
-          formats: ['ttf', 'eot', 'woff', 'woff2', 'svg'],
-          timestamp,
-          log: () => {},
-        }),
-      )
-      .on('glyphs', glyphs => {
-        const options = {
-          className,
-          fontName,
-          fontPath: '../fonts/',
-          glyphs: glyphs.map(glyph => ({
-            name: glyph.name,
-            codepoint: glyph.unicode[0].charCodeAt(0),
-          })),
-        };
-        gulp
-          .src(`templates/${template}.css`)
-          .pipe(consolidate('lodash', options))
-          .pipe(rename({ basename: fontName }))
-          .pipe(gulp.dest('dist-css/css/'));
-
-        gulp
-          .src(`templates/${template}.scss`)
-          .pipe(consolidate('lodash', options))
-          .pipe(rename({ basename: fontName }))
-          .pipe(gulp.dest('dist-css/scss/'));
-
-        gulp
-          .src(`templates/${templateInternal}.scss`)
-          .pipe(consolidate('lodash', options))
-          .pipe(rename({ basename: fontName }))
-          .pipe(gulp.dest('src/components/icon/generated'));
-
-        gulp
-          .src(`templates/${template}.html`)
-          .pipe(consolidate('lodash', options))
-          .pipe(rename({ basename: 'sample' }))
-          .pipe(gulp.dest('dist-css/'));
-
-        gulp
-          .src(`templates/${template}.json`)
-          .pipe(consolidate('lodash', options))
-          .pipe(rename({ basename: 'sample' }))
-          .pipe(gulp.dest('dist-css/'));
-      })
-      .pipe(gulp.dest('dist-css/fonts/')),
-  ),
+  gulp.series(() => gulp.src('./svg/*.svg').pipe(gulp.dest('dist-css/svg'))),
 );
--- stencil.config.ts
+++ stencil.config.ts
@@ -13,20 +13,8 @@ export const config: Config = {
       esmLoaderPath: '../loader',
       copy: [
         {
-          src: '../dist-css/css',
-          dest: '../../dist/css',
-        },
-        {
-          src: '../dist-css/fonts',
-          dest: '../../dist/fonts',
-        },
-        {
           src: '../dist-css/svg',
           dest: '../../dist/svg',
-        },
-        {
-          src: '../dist-css/sample.html',
-          dest: '../../dist/sample.html',
         },
         {
           src: '../dist-css/sample.json',
@@ -41,14 +29,6 @@ export const config: Config = {
       type: 'www',
       serviceWorker: null, // disable service workers
       copy: [
-        {
-          src: '../dist-css/css',
-          dest: 'build/css',
-        },
-        {
-          src: '../dist-css/fonts',
-          dest: 'build/fonts  ',
-        },
         {
           src: '../svg',
           dest: 'build/svg  ',
