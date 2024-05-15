393c93299d97fc46584c06c153bb75cf2e74f13e
Daniel Leroux <daniel.leroux@siemens.com>
2024-05-15 18:10:45 +0200

    remove breaking change

--- scripts/build-icons.ts
+++ scripts/build-icons.ts
@@ -176,7 +176,7 @@ async function buildIcons() {
     ),
 
     writeIconSampleJson(iconCollection, path.join(rootPath, 'build-dist'), version),
-
+    writeGlobalCSSFile(path.join(rootPath, 'build-dist', 'css', 'ix-icons.css')),
     fs.writeFile(
       iconsPkgPath,
       JSON.stringify(
@@ -248,6 +248,28 @@ function getDataUrl(svgData: string) {
   }
   svg = svg.replace(/"/g, "'");
   return `"data:image/svg+xml;utf8,${svg}"`;
+}
+
+async function writeGlobalCSSFile(targetPath: string) {
+  // Write the global css file to keep the application compiling after update to 2.0.0
+  fs.ensureDirSync(path.join(targetPath, '..'));
+
+  return fs.writeFile(
+    targetPath,
+    `
+/*
+* SPDX-FileCopyrightText: 2023 Siemens AG
+*
+* SPDX-License-Identifier: MIT
+*
+* This source code is licensed under the MIT license found in the
+* LICENSE file in the root directory of this source tree.
+*/
+/*
+* Deprecated since 2.0.0 no global css file is necessary.
+*/
+    `,
+  );
 }
 
 buildIcons();
