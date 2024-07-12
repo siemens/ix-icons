2fa9b9aeb2cac1d96e21f101cc216a0cda52989c
Daniel Leroux <daniel.leroux@siemens.com>
2024-07-12 15:28:44 +0200

    fix asset path

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export type { Components, JSX } from './components';
 export * from './components/icon/icon';
 export { preloadIconMap } from './legacy-support';
+export { setAssetPath } from '@stencil/core';
--- src/legacy-support.ts
+++ src/legacy-support.ts
@@ -6,8 +6,9 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  */
+import { setAssetPath } from '@stencil/core';
 import * as icons from './components/icon/icons';
-import { getIconCacheMap, parseSVGDataContent } from './components/icon/resolveIcon';
+import { getIconCacheMap, parseSVGDataContent, getAssetUrl } from './components/icon/resolveIcon';
 
 function fromCamelToKebabCase(camelString: string): string {
   let kebabString = '';
@@ -29,6 +30,16 @@ export function preloadIconMap() {
   Object.keys(icons).forEach(key => {
     const fileName = fromCamelToKebabCase(key.substring('icon'.length));
     const icon = icons[key];
-    iconsCache.set(`/build/svg/${fileName}.svg`, parseSVGDataContent(icon));
+
+    let url: string = '';
+    try {
+      url = getAssetUrl(fileName);
+    } catch (error) {
+      setAssetPath(`${window.location.origin}/`);
+    } finally {
+      url = getAssetUrl(fileName);
+    }
+
+    iconsCache.set(url, parseSVGDataContent(icon));
   });
 }
