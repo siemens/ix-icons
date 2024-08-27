f4f871cec912370a9f0b66cfb4cf699a4e9a3dd2
Daniel Leroux <daniel.leroux@siemens.com>
2024-08-27 11:35:50 +0200

    fix: expose all asset utils

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
 export type { Components, JSX } from './components';
 export * from './components/icon/icon';
-export { setAssetPath } from '@stencil/core';
+export { setAssetPath, getAssetPath } from '@stencil/core';
